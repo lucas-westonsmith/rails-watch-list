16ca8ae86cebfcfab61eb6934f19537c3b3c9dd6
Lucas Weston Smith <lucas.westonsmith@essec.edu>
2024-11-15 12:59:27 +0000

    Add dotenv-rails

--- Gemfile
+++ Gemfile
@@ -26,6 +26,15 @@ gem "stimulus-rails"
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 gem "jbuilder"
 
+gem "sassc-rails"
+gem "simple_form"
+gem "bootstrap", "~> 5.2"
+gem "font-awesome-sass", "~> 6.1"
+gem "autoprefixer-rails"
+gem "cloudinary"
+gem "dotenv-rails"
+
+
 # Use Redis adapter to run Action Cable in production
 # gem "redis", ">= 4.0.1"
 
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,12 +1,6 @@
 class Movie < ApplicationRecord
+  has_many :bookmarks
+
   validates :title, presence: true, uniqueness: true
   validates :overview, presence: true
-  has_many :bookmarks
-  before_destroy :ensure_no_bookmarks
-
-  private
-
-  def ensure_no_bookmarks
-    raise ActiveRecord::InvalidForeignKey, 'Cannot delete movie with associated bookmarks' if bookmarks.exists?
-  end
 end
